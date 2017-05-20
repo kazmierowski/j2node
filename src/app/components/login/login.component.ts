343dc7a85b18784c0be32d4effeb5cfb64a1d9bd
Kamil Kazmierowski <kamil.kazmierowski@gmail.com>
2017-05-20 21:43:08 +0100

    cleaning

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,40 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LoginService} from './login.service';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  providers: [LoginService]
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
-  public useremail: string;
-  public userpass: string;
-  public communicat: string;
-  constructor(private loginService: LoginService, private router: Router) {}
-  
-  public login() {
-    
-      var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-      if (this.useremail != "" && (this.useremail.length <= 5 || !EMAIL_REGEXP.test(this.useremail))) {
+    public useremail: string;
+    public userpass: string;
+    public communicat: string;
+
+    constructor(private loginService: LoginService, private router: Router) {
+    }
+
+    public login() {
+
+        const EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+
+        if (this.useremail != "" && (this.useremail.length <= 5 || !EMAIL_REGEXP.test(this.useremail))) {
             this.communicat = 'Incorrect email format!';
-      } else if(this.useremail === undefined || this.userpass === undefined || this.useremail == '' || this.userpass == ''){
-          this.communicat = 'Fill both fields!';
+        } else if (this.useremail === undefined || this.userpass === undefined || this.useremail == '' || this.userpass == '') {
+            this.communicat = 'Fill both fields!';
         } else {
-          this.loginService.login(this.useremail, this.userpass).subscribe(
-        (res) => {
-            if (res !== false) {
-                this.router.navigate(['/board']);
-            } else {
-                this.communicat = 'Incorrect password or login!';
-            }
-            }
-          );
+            this.loginService.login(this.useremail, this.userpass).subscribe(
+                (res) => {
+                    if (res !== false) {
+                        this.router.navigate(['/board']);
+                    } else {
+                        this.communicat = 'Incorrect password or login!';
+                    }
+                }
+            );
         }
-  }
+    }
 
-  ngOnInit() {
-  }
-
-
+    ngOnInit() {
+    }
 }
