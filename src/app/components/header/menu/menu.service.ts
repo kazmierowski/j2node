535553e6ac3fb06606fe3a3ce0ca588ec502d916
Kamil Kaźmierowski <kamil.kazmierowski@gmail.com>
2017-06-05 10:06:55 +0000

    cleaning in menu.service.ts

--- src/app/components/header/menu/menu.service.ts
+++ src/app/components/header/menu/menu.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
-import {LoginService} from "../../login/login.service";
-import {Router} from "@angular/router";
 
 @Injectable()
 export class MenuService {
 
   private menuFieldsUrl = './test/menu.json';
 
-  constructor(private http: Http, private loginService: LoginService, private router: Router) { }
+  constructor(private http: Http,) { }
 
   public getMenuFields() {
     return this.http.get(this.menuFieldsUrl)
